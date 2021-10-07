8adcec12364fb6bae2298ddbe485de9f74c5ab92
EErikas <EErikas@github.com>
2021-10-07 23:47:08 +0300

    made dragons public

--- Module_3/dragons/src/fantasy_dragon.hpp
+++ Module_3/dragons/src/fantasy_dragon.hpp
@@ -9,12 +9,12 @@
  * Public members of Dragon should be public in FantasyDragon as well.
  */
 
-class FantasyDragon : public Dragon {
-    FantasyDragon(const std::string& name, size_t age, size_t size);
-
-    virtual void Eat(std::list<Food> &food);
-    virtual void Hoard(std::list<Treasure> &treasure);
+class FantasyDragon : public Dragon
+{
+public:
+    FantasyDragon(const std::string &name, size_t age, size_t size);
+    void Eat(std::list<Food> &food);
+    void Hoard(std::list<Treasure> &treasure);
     bool EatHerbs(Food &fd);
     bool HoardPotion(Treasure &tr);
-
 };
--- Module_3/dragons/src/magic_dragon.hpp
+++ Module_3/dragons/src/magic_dragon.hpp
@@ -10,12 +10,12 @@
  * Public members of Dragon should be public in MagicDragon as well.
  */
 
-class MagicDragon : public Dragon {
-    MagicDragon(const std::string& name, size_t age, size_t size);
-
-    virtual void Eat(std::list<Food> &food);
-    virtual void Hoard(std::list<Treasure> &treasure);
+class MagicDragon : public Dragon
+{
+public:
+    MagicDragon(const std::string &name, size_t age, size_t size);
+    void Eat(std::list<Food> &food);
+    void Hoard(std::list<Treasure> &treasure);
     bool EatHerbs(Food &fd);
     bool HoardPotion(Treasure &tr);
-
 };
