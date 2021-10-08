d7429ff3b5d1f40c3edd15f6f71aa77b3d6819b0
linroad <linroad@hotmail.com>
2021-10-08 11:26:25 +0300

    fantasy_dragon fixed

--- Module_3/dragons/src/fantasy_dragon.cpp
+++ Module_3/dragons/src/fantasy_dragon.cpp
@@ -1,11 +1,18 @@
 #include "fantasy_dragon.hpp"
+#include <algorithm>
 
 // Define FantasyDragon's methods here
 FantasyDragon::FantasyDragon(const std::string &name, size_t age, size_t size) : Dragon(name, age, size) {}
 
 void FantasyDragon::Eat(std::list<Food> &food)
 {
-    food.remove_if(&FantasyDragon::EatHerbs);
+    // food.remove_if(FantasyDragon::EatHerbs);
+    // std::remove_if(food.begin(),food.end(),&EatHerbs);
+    for (auto i : food) {
+        if (EatHerbs(i)) {
+            food.remove(i);
+        }
+    }
 }
 
 bool FantasyDragon::EatHerbs(Food &fd)
@@ -13,7 +20,7 @@ bool FantasyDragon::EatHerbs(Food &fd)
     if (fd.type == People or fd.type == PeopleFood)
     {
         std::cout << "Fantasy dragon ate: " << fd.name << std::endl;
-        this -> size_++;
+        this->size_++;
         return true;
     }
     else
@@ -24,7 +31,12 @@ bool FantasyDragon::EatHerbs(Food &fd)
 
 void FantasyDragon::Hoard(std::list<Treasure> &treasure)
 {
-    treasure.remove_if(&FantasyDragon::HoardPotion);
+    // treasure.remove_if(&FantasyDragon::HoardPotion);
+    for (auto i : treasure) {
+        if (HoardPotion(i)) {
+            treasure.remove(i);
+        }
+    }
 }
 
 bool FantasyDragon::HoardPotion(Treasure &tr)
@@ -32,7 +44,7 @@ bool FantasyDragon::HoardPotion(Treasure &tr)
     if (tr.type == Jewellery)
     {
         std::cout << "Fantasy dragon received: " << tr.name << std::endl;
-        this -> treasure_.push_back(tr);
+        this->treasure_.push_back(tr);
         return true;
     }
     else
